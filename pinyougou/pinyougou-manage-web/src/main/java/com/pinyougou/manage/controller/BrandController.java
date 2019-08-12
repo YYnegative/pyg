cabf620928229ecaf9e603799c45a14ee2179b68
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-12 11:19:27 +0800

    整合品牌业务并测试BaseMapper

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -5,6 +5,7 @@ import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,11 +18,24 @@ public class BrandController {
     private BrandService brandService;
 
     /**
+     * 根据分页参数分页查询品牌列表
+     * @param pageNum 页号
+     * @param pageSize 页大小
+     * @return 品牌列表
+     */
+    @GetMapping("/testPage")
+    public List<TbBrand> testPage(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                  @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize) {
+        return brandService.testPage(pageNum, pageSize);
+    }
+
+    /**
      * 查询品牌列表
+     *
      * @return 品牌列表
      */
     @GetMapping("/findAll")
-    public List<TbBrand> findAll(){
+    public List<TbBrand> findAll() {
         return brandService.queryAll();
     }
 }
