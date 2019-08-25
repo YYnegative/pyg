c1a48a59375b973c3138aa1ed9a58bceac2db33f
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-25 11:00:19 +0800

    ElasticSearch-嵌套域

--- itcast-elasticsearch/src/test/java/cn/itcast/es/ElasticSearchTest.java
+++ itcast-elasticsearch/src/test/java/cn/itcast/es/ElasticSearchTest.java
@@ -2,6 +2,9 @@ package cn.itcast.es;
 
 import cn.itcast.es.dao.ItemDao;
 import com.pinyougou.pojo.TbItem;
+import org.apache.lucene.search.join.ScoreMode;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.NestedQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +17,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-es.xml")
@@ -49,6 +54,12 @@ public class ElasticSearchTest {
         item.setSeller("一加");
         item.setImage("https://item.jd.com/100003344497.html");
         item.setUpdateTime(new Date());
+
+        //设置嵌套域
+        Map<String, String> specMap = new HashMap<String, String>();
+        specMap.put("颜色", "曜岩灰");
+        specMap.put("内存大小", "64G");
+        item.setSpecMap(specMap);
 
         itemDao.save(item);
     }
@@ -149,6 +160,58 @@ public class ElasticSearchTest {
             System.out.println(item);
         }
     }
+
+
+    //嵌套域
+    @Test
+    public void nestedQuery(){
+
+        //在spring data es中所有的对象几乎都有一个自身的构造对象
+
+        //查询构造对象
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+
+        //设置查询条件对象
+        queryBuilder.withQuery(QueryBuilders.matchQuery("title", "手机"));
+
+        //添加过滤查询
+
+        //组合查询，可以设置多个条件
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+        /**
+         *  参数 1 ：路径；嵌套对象；实体类中属性名称
+         *  参数 2 ：查询条件对象
+         *  参数 3 ： ScoreMode  定义里嵌套对象计算的分数与当前查询分数的处理方式， max
+         表示取最大。查询到的结果中有多个文档，每个文档都有得分，max 表示选择最大的分值作
+         为本次查询的_score 分值
+         */
+        NestedQueryBuilder nestedQueryBuilder1 =
+                new NestedQueryBuilder("specMap", QueryBuilders.wildcardQuery("specMap.颜色.keyword", "曜岩*"), ScoreMode.Max);
+        boolQueryBuilder.must(nestedQueryBuilder1);
+
+        NestedQueryBuilder nestedQueryBuilder2 =
+                new NestedQueryBuilder("specMap", QueryBuilders.wildcardQuery("specMap.内存大小.keyword", "64G"), ScoreMode.Max);
+        boolQueryBuilder.must(nestedQueryBuilder2);
+
+        //设置组合查询为过滤查询条件
+        queryBuilder.withFilter(boolQueryBuilder);
+
+        //构造查询对象
+        NativeSearchQuery query = queryBuilder.build();
+
+        //搜索
+        AggregatedPage<TbItem> aggregatedPage = esTemplate.queryForPage(query, TbItem.class);
+
+        //分页信息
+        System.out.println("总记录数为：" + aggregatedPage.getTotalElements());
+        System.out.println("总页数为：" + aggregatedPage.getTotalPages());
+
+        for (TbItem item : aggregatedPage.getContent()) {
+            System.out.println(item);
+        }
+    }
+
 
 }
 
