5c2c5d52ed73765d90e1acbd563389dc67c465f3
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-28 10:49:18 +0800

    Freemarker指令-空值处理

--- itcast-freemarker/src/test/java/cn/itcast/freemarker/FreemarkerTest.java
+++ itcast-freemarker/src/test/java/cn/itcast/freemarker/FreemarkerTest.java
@@ -5,8 +5,7 @@ import freemarker.template.Template;
 import org.junit.Test;
 
 import java.io.FileWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FreemarkerTest {
 
@@ -26,6 +25,24 @@ public class FreemarkerTest {
         Map<String, Object> dataModel = new HashMap<>();
         dataModel.put("name", "heima");
         dataModel.put("message", "你好！黑马。");
+
+        //列表
+        List<Map<String, Object>> goodsList = new ArrayList<>();
+
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("name", "柚子");
+        map1.put("price", "6.0");
+        goodsList.add(map1);
+
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("name", "葡萄");
+        map2.put("price", "12.2");
+        goodsList.add(map2);
+
+        dataModel.put("goodsList", goodsList);
+        dataModel.put("today", new Date());
+        dataModel.put("number", 123456789L);
+
 
         //3. 输出；参数1：数据map，参数2：输出对象
         //创建输出对象
