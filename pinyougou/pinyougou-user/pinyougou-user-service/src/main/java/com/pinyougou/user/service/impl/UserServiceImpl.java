6358746bafac0a30cf47e2c64c56cc6baf6a62b1
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-31 12:44:43 +0800

    发送短信验证码实现

--- pinyougou/pinyougou-user/pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/UserServiceImpl.java
+++ pinyougou/pinyougou-user/pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/UserServiceImpl.java
@@ -7,17 +7,43 @@ import com.pinyougou.mapper.UserMapper;
 import com.pinyougou.pojo.TbUser;
 import com.pinyougou.user.service.UserService;
 import com.pinyougou.service.impl.BaseServiceImpl;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import tk.mybatis.mapper.entity.Example;
 
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserServiceImpl extends BaseServiceImpl<TbUser> implements UserService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private ActiveMQQueue itcastSmsQueue;
+
+    @Value("${signName}")
+    private String signName;
+
+    @Value("${templateCode}")
+    private String templateCode;
 
     @Override
     public PageInfo<TbUser> search(Integer pageNum, Integer pageSize, TbUser user) {
@@ -36,6 +62,31 @@ public class UserServiceImpl extends BaseServiceImpl<TbUser> implements UserServ
 
         List<TbUser> list = userMapper.selectByExample(example);
         return new PageInfo<>(list);
+    }
+
+    @Override
+    public void sendSmsCode(String phone) {
+        //- 1、生成6位随机数字作为要发送的验证码; Math.random()生成0-1之间的小数 TODO
+        String code = (long)(Math.random()*1000000)+"";
+        System.out.println("验证码为：" + code);
+
+        //- 2、将验证码存入到redis中，手机号为key；并设置过期时间5分钟
+        redisTemplate.boundValueOps(phone).set(code);
+        redisTemplate.boundValueOps(phone).expire(5, TimeUnit.MINUTES);
+
+        //- 3、利用JmsTemplate发送（mobile手机号，signName签名，templateCode模版编号，templateParam模版参数）到ActiveMQ的 itcast_sms_queue
+        jmsTemplate.send(itcastSmsQueue, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                MapMessage mapMessage = session.createMapMessage();
+                mapMessage.setString("mobile", phone);
+                mapMessage.setString("signName", signName);
+                mapMessage.setString("templateCode", templateCode);
+                //{"code":123123}
+                mapMessage.setString("templateParam", "{\"code\":"+code+"}");
+                return mapMessage;
+            }
+        });
     }
 
 }
