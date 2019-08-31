a633b69d7cad8cf1e10dc91fa9bf6b1e6e6e41a5
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-31 09:48:16 +0800

    Spring Boot修改&读取配置项

--- itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/HelloController.java
+++ itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/HelloController.java
@@ -1,5 +1,8 @@
 package cn.itcast.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/info")
+    @Value("${url}")
+    private String url;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/info2")
     public String info(){
-        return "Hello Spring Boot！";
+        System.out.println("url = " + environment.getProperty("url"));
+
+        return "Hello Spring Boot！ url = " + url;
     }
 }
