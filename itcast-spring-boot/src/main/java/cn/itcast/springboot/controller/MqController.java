ee50ede2c00415a290c4643fd3019f70e34b54df
liaojianbin <meinitcasthavefun@foxmail.com>
2019-08-31 11:24:14 +0800

    短信微服务系统测试

--- itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/MqController.java
+++ itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/MqController.java
@@ -29,4 +29,20 @@ public class MqController {
         }
         return "发送MQ消息成功！发送的队列为spring.boot.queue";
     }
+
+    @GetMapping("/sendSms")
+    public String sendSmsMQMsg(){
+        try {
+            Map<String, Object> map = new HashMap<String,Object>();
+            map.put("mobile", "15707588210");
+            map.put("signName", "黑马");
+            map.put("templateCode", "SMS_125018593");
+            map.put("templateParam", "{\"code\":\"654321\"}");
+
+            jmsMessagingTemplate.convertAndSend("itcast_sms_queue", map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "发送MQ消息成功！发送的队列为itcast_sms_queue";
+    }
 }
