c985a27f911941750bdfcff886d8a317f3eea811
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-03 11:11:49 +0800

    未登录-购物车列表加载-后端实现

--- pinyougou/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
+++ pinyougou/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -1,16 +1,57 @@
 package com.pinyougou.cart.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.pinyougou.common.util.CookieUtils;
+import com.pinyougou.vo.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequestMapping("/cart")
 @RestController
 public class CartController {
+    //在浏览器中的cookie的名称
+    private static final String COOKIE_CART_LIST = "PYG_CART_LIST";
+
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
+
+    /**
+     * 登录与未登录情况下
+     * 获取购物车数据
+     * @return 购物车列表
+     */
+    @GetMapping("/findCartList")
+    public List<Cart> findCartList(){
+        //判断用户是否登录；如果没有登录则用户名为 anonymousUser
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        if ("anonymousUser".equals(username)) {
+            //未登录- 从cookie中获取购物车
+            String cartListJsonStr = CookieUtils.getCookieValue(request, COOKIE_CART_LIST, true);
+            List<Cart> cookieCartList;
+            if (!StringUtils.isEmpty(cartListJsonStr)) {
+                cookieCartList = JSON.parseArray(cartListJsonStr, Cart.class);
+                return cookieCartList;
+            } else {
+                return new ArrayList<>();
+            }
+        } else {
+            //已登录- 从redis中获取购物车
+            return null;
+        }
+    }
 
     /**
      * 获取当前登录用户信息
