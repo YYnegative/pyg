6b6e847be7f2d3868eb5eb6bfa85d41a0489c263
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-03 10:39:52 +0800

    未登录-购物车列表加载-前端实现

--- pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -1,9 +1,17 @@
 var app = new Vue({
     el:"#app",
     data: {
-        username:""
+        username:"",
+        //购物车列表
+        cartList:[]
     },
     methods: {
+        //查询购物车数据
+        findCartList:function(){
+          axios.get("cart/findCartList.do").then(function (response) {
+              app.cartList = response.data;
+          });
+        },
         //获取用户名
         getUsername: function () {
             axios.get("cart/getUsername.do").then(function (response) {
@@ -13,5 +21,7 @@ var app = new Vue({
     },
     created(){
         this.getUsername();
+        //查询购物车列表
+        this.findCartList();
     }
 });
